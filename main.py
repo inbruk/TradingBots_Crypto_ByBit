447d8c317bfc27d54ddd9a69d9dede13150a0d99
inbruk <35333872+inbruk@users.noreply.github.com>
2021-01-27 06:21:37 +0300

    ....

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import json
 import requests
 import datetime
 import pandas as pd
+from pconst import const
 from IPython.core.display import display
 
-public_api_url = 'https://api.bybit.com/v2/public/'
+const.PUBLIC_API_URL = 'https://api.bybit.com/v2/public/'
 
-server_access_name = os.getenv('BYBIT_NAME')
-server_access_api_key = os.getenv('BYBIT_API_KEY')
-server_access_secret_code = os.getenv('BYBIT_SECRET_CODE')
+const.SERVER_ACCESS_NAME = os.getenv('BYBIT_NAME')
+const.SERVER_ACCESS_API_KEY = os.getenv('BYBIT_API_KEY')
+const.SERVER_ACCESS_SECRET_CODE = os.getenv('BYBIT_SECRET_CODE')
+
+const.START_UTC = 1607648220
 
 # display(server_access_name)
 # display(server_access_api_key)
 # display(server_access_secret_code)
 
 
-def load_hour_values(symbol_str, begin_utc):
+def load_hour_values_from_server(symbol_str, begin_utc):
 
     req = requests.get(
-        public_api_url + 'kline/list',
+        const.PUBLIC_API_URL + 'kline/list',
         {
             'symbol':symbol_str,
             'interval':1,
@@ -40,38 +43,58 @@ def load_hour_values(symbol_str, begin_utc):
     return df
 
 
-def merge_and_save_to_cache(symbol_str, new_df):
+def get_cache_filename(symbol_str):
+    return r'data/' + symbol_str.lower() + '.csv'
+
+
+def load_values_from_cache(symbol_str):
 
     df = pd.DataFrame(columns=['dt', 'open', 'high', 'low', 'close', 'volume', 'turnover'])
-    filename = r'data/' + symbol_str.lower() + '.csv'
+    filename = get_cache_filename(symbol_str)
 
-    # if exists load cached data
     if os.path.exists(filename):
        df = pd.read_csv(filename)
-
-    # insert new data and remove doubles by dt
-    df = df.append(new_df, ignore_index=True)
-    df.drop_duplicates(subset='dt', keep='first', inplace=True)
-
-    df.to_csv(filename, index = False, header=True)
 
     return df
 
 
+def get_prev_minute_utc():
+    curr_datetime = datetime.datetime.now()
+    curr_datetime = datetime.datetime(curr_datetime.year, curr_datetime.month, curr_datetime.day, curr_datetime.hour, curr_datetime.minute, 0)
+    curr_datetime -= datetime.timedelta(minutes=1)
+    curr_utc = curr_datetime.timestamp()
+    return curr_utc
+
+
+def get_values_and_update_cache(symbol_str):
+
+    df = load_values_from_cache(symbol_str)
+    if len(df) > 0:
+        current_start_utc = df['dt'].max()
+    else:
+       current_start_utc = const.START_UTC
+
+    curr_utc = get_prev_minute_utc()
+    while True:
+        new_df = load_hour_values_from_server(symbol_str, current_start_utc)
+        df = df.append(new_df, ignore_index=True)
+        df.drop_duplicates(subset='dt', keep='first', inplace=True)
+        current_end_utc = df['dt'].max()
+        if current_end_utc >= curr_utc:
+            break
+        else:
+            current_start_utc = current_end_utc
+
+    filename = get_cache_filename(symbol_str)
+    df.to_csv(filename, index = False, header=True)
+
+    return df
+
 print( '--------------------------------------')
-new_df = load_hour_values('BTCUSD',1607648220)
-res_df = merge_and_save_to_cache('BTCUSD', new_df)
+
+res_df = get_values_and_update_cache(symbol_str):
 display(res_df)
 
-
 print( '--------------------------------------')
-curr_datetime = datetime.datetime.now()
-display(curr_datetime)
 
-curr_datetime += datetime.timedelta(hours=1)
-display(curr_datetime)
-
-
-curr_timestamp = curr_datetime.timestamp()
-display(curr_timestamp)
 
