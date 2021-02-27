4298429397c7a4ab6e277d0c3bbf1e489ab735ba
inbruk <35333872+inbruk@users.noreply.github.com>
2021-02-27 07:12:05 +0300

    ......

--- update_equations.py
+++ update_equations.py
@@ -28,6 +28,9 @@ const.avg31_col_name = 'avg31'
 const.avg181_col_name = 'avg181'
 const.avg1441_col_name = 'avg1441'
 
+const.open_col_name = 'open'
+const.close_col_name = 'close'
+
 
 def get_cache_filename(symbol_str):
     return r'data/' + symbol_str.lower() + '.csv'
@@ -37,47 +40,65 @@ def get_output_filename(symbol_str):
     return r'data/' + symbol_str.lower() + '_' + const.SUFFIX + '.csv'
 
 
-def update_eq_value(in_df):
+def update_eq_value(in_df, old_df):
 
     out_df = pd.DataFrame(columns=[const.dt_col_name, const.value_col_name, const.delta1_col_name,
                                    const.delta2_col_name, const.avg7_col_name, const.avg31_col_name,
                                    const.avg181_col_name, const.avg1441_col_name])
 
     for index, item in in_df.iterrows():
-        new_value = ( item['open'] + item['close'] ) / 2.0
         new_row = {
             const.dt_col_name: item[const.dt_col_name],
-            const.value_col_name: new_value,
-            const.delta1_col_name: 0,
-            const.delta2_col_name: 0,
-            const.avg7_col_name: 0,
-            const.avg31_col_name: 0,
-            const.avg181_col_name: 0,
-            const.avg1441_col_name: 0
+            const.value_col_name: 0.0,
+            const.delta1_col_name: 0.0,
+            const.delta2_col_name: 0.0,
+            const.avg7_col_name: 0.0,
+            const.avg31_col_name: 0.0,
+            const.avg181_col_name: 0.0,
+            const.avg1441_col_name: 0.0
         }
         out_df = out_df.append(new_row, ignore_index=True)
+
+    in_len = in_df[const.dt_col_name].size
+    old_len = old_df[const.dt_col_name].size
+
+    for x in range(0, old_len):
+        out_df.at[x, const.value_col_name] = old_df.at[x, const.value_col_name]
+
+    for x in range(old_len, in_len):
+        out_df.at[x, const.value_col_name] = (in_df.at[x, const.open_col_name] + in_df.at[x, const.close_col_name]) / 2.0
 
     return out_df
 
 
-def update_eq_delta1(out_df):
+def update_eq_delta1(old_df, out_df):
 
-    lenght = out_df[const.dt_col_name].size
-    out_df.at[0,const.delta1_col_name] = 0.0
+    out_len = out_df[const.dt_col_name].size
+    old_len = old_df[const.dt_col_name].size
 
-    for x in range(1, lenght):
+    out_df.at[0, const.delta1_col_name] = 0.0
+
+    for x in range(1, old_len):
+        out_df.at[x, const.delta1_col_name] = old_df.at[x, const.delta1_col_name]
+
+    for x in range(old_len, out_len):
         out_df.at[x, const.delta1_col_name] = out_df.at[x, const.value_col_name] - out_df.at[x-1, const.value_col_name]
 
     return out_df
 
 
-def update_eq_delta2(out_df):
+def update_eq_delta2(old_df, out_df):
 
-    lenght = out_df[const.dt_col_name].size
-    out_df.at[0,const.delta2_col_name] = 0.0
-    out_df.at[1,const.delta2_col_name] = 0.0
+    out_len = out_df[const.dt_col_name].size
+    old_len = old_df[const.dt_col_name].size
 
-    for x in range(2, lenght):
+    out_df.at[0, const.delta2_col_name] = 0.0
+    out_df.at[1, const.delta2_col_name] = 0.0
+
+    for x in range(2, old_len):
+        out_df.at[x, const.delta2_col_name] = old_df.at[x, const.delta2_col_name]
+
+    for x in range(old_len, out_len):
         out_df.at[x, const.delta2_col_name] = out_df.at[x, const.delta1_col_name] - out_df.at[x-1, const.delta1_col_name]
 
     return out_df
@@ -102,12 +123,21 @@ def calc_avg_value(out_df, index, hwnd_size, full_length):
     return sumv/count
 
 
-def update_eq_avg(out_df, hwnd_size, col_name):
+def update_eq_avg(old_df, out_df, hwnd_size, col_name):
 
-    ln = out_df[const.dt_col_name].size
+    out_len = out_df[const.dt_col_name].size
+    old_len = old_df[const.dt_col_name].size
+    full_wnd_size = (2*hwnd_size+1)
+    if old_len < full_wnd_size:
+        old_len = 0
+    else:
+        old_len -= hwnd_size
 
-    for x in range(0, ln):
-        out_df.at[x, col_name] = calc_avg_value(out_df, x, hwnd_size, ln)
+    for x in range(0, old_len):
+        out_df.at[x, col_name] = old_df.at[x, col_name]
+
+    for x in range(old_len, out_len):
+        out_df.at[x, col_name] = calc_avg_value(out_df, x, hwnd_size, out_len)
 
     return out_df
 
@@ -118,28 +148,36 @@ def update_equations(symbol_str):
     in_df = pd.read_csv(in_file_name)
     print('.', end='')
 
-    out_df = update_eq_value(in_df)
-    print('.', end='')
-
-    out_df = update_eq_delta1(out_df)
-    print('.', end='')
-
-    out_df = update_eq_delta2(out_df)
-    print('.', end='')
-
-    out_df = update_eq_avg(out_df, const.avg7_hwnd, const.avg7_col_name)
-    print('.', end='')
-
-    out_df = update_eq_avg(out_df, const.avg31_hwnd, const.avg31_col_name)
-    print('.', end='')
-
-    out_df = update_eq_avg(out_df, const.avg181_hwnd, const.avg181_col_name)
-    print('.', end='')
-
-    out_df = update_eq_avg(out_df, const.avg1441_hwnd, const.avg1441_col_name)
-    print('.', end='')
-
     out_file_name = get_output_filename(symbol_str)
+    if os.path.exists(out_file_name):
+        old_df = pd.read_csv(out_file_name)
+    else:
+        old_df = pd.DataFrame(columns=[const.dt_col_name, const.value_col_name, const.delta1_col_name,
+                                       const.delta2_col_name, const.avg7_col_name, const.avg31_col_name,
+                                       const.avg181_col_name, const.avg1441_col_name])
+    print('.', end='')
+
+    out_df = update_eq_value(in_df, old_df)
+    print('.', end='')
+
+    out_df = update_eq_delta1(old_df, out_df)
+    print('.', end='')
+
+    out_df = update_eq_delta2(old_df, out_df)
+    print('.', end='')
+
+    out_df = update_eq_avg(old_df, out_df, const.avg7_hwnd, const.avg7_col_name)
+    print('.', end='')
+
+    out_df = update_eq_avg(old_df, out_df, const.avg31_hwnd, const.avg31_col_name)
+    print('.', end='')
+
+    out_df = update_eq_avg(old_df, out_df, const.avg181_hwnd, const.avg181_col_name)
+    print('.', end='')
+
+    out_df = update_eq_avg(old_df, out_df, const.avg1441_hwnd, const.avg1441_col_name)
+    print('.', end='')
+
     out_df.to_csv(out_file_name, index=False, header=True)
     print('.')
 
