3942cea9eaf99c6be92795d0ef057944ddd12f6d
inbruk <35333872+inbruk@users.noreply.github.com>
2021-04-18 19:18:46 +0300

    ...................

--- update_orders.py
+++ update_orders.py
@@ -17,7 +17,7 @@ def get_orders_filename(symbol_str):
     return r'data/' + symbol_str.lower() + '_' + const.ORDERS + '.csv'
 
 
-def check_order_open_close(out_df, x, o_now, o_buy, o_open, o_change):
+def check_order_open_close(out_df, x, o_now, o_buy):
 
     delta1441 = out_df.at[x, const.avg1441_col_name] - out_df.at[x-1, const.avg1441_col_name]
     delta181 = out_df.at[x, const.avg181_col_name] - out_df.at[x-1, const.avg181_col_name]
@@ -27,33 +27,28 @@ def check_order_open_close(out_df, x, o_now, o_buy, o_open, o_change):
     price = out_df.at[x, const.value_col_name]
 
     o_change = False
-    o_open = False
 
     if not o_now:
         if delta1441 > 0 and delta181 > 0 and delta31 > 0 and delta7 > 0:
             o_change = True
-            o_open = True
             o_now = True
             o_buy = True
 
         if delta1441 < 0 and delta181 < 0 and delta31 < 0 and delta7 < 0:
             o_change = True
-            o_open = True
             o_now = True
             o_buy = False
     else:
         if o_buy:
             if (delta1441 + delta181) < 0:
                 o_change = True
-                o_open = False
                 o_now = False
         else:
             if (delta1441 + delta181) > 0:
                 o_change = True
-                o_open = False
                 o_now = False
 
-    return o_now, o_buy, o_open, o_change
+    return o_now, o_buy, o_change
 
 
 def fill_equation_values(out_df, x, o_now, o_buy, mean_value, min_value, max_value):
@@ -69,10 +64,13 @@ def fill_equation_values(out_df, x, o_now, o_buy, mean_value, min_value, max_val
     return out_df
 
 
-def fill_order_values(ord_df, o_buy, beg_dt, beg_val, end_dt, end_val):
+def fill_order_values(ord_df, o_open, o_buy, beg_dt, beg_val, end_dt, end_val):
 
     len = ord_df[const.type_col_name].size
-    pos = len
+    if o_open:
+        pos = len # add new row
+    else:
+        pos = len - 1 # insert values into last row
 
     if o_buy:
         ord_df.at[pos,const.type_col_name] = const.order_type_buy
@@ -81,31 +79,41 @@ def fill_order_values(ord_df, o_buy, beg_dt, beg_val, end_dt, end_val):
 
     ord_df.at[pos,const.open_dt_col_name] = beg_dt
     ord_df.at[pos,const.open_price_col_name] = beg_val
-    ord_df.at[pos,const.close_dt_col_name] = end_dt
-    ord_df.at[pos,const.close_price_col_name] = end_val
 
-
-    if o_buy:
-        d_price = end_val - beg_val
+    if o_open:
+        ord_df.at[pos, const.close_dt_col_name] = 0.0
+        ord_df.at[pos, const.close_price_col_name] = 0.0
+        ord_df.at[pos, const.delta_price] = 0.0
+        ord_df.at[pos, const.delta_price_prc] = 0.0
+        ord_df.at[pos, const.profit] = 0.0
+        ord_df.at[pos, const.profit_prc] = 0.0
+        ord_df.at[pos, const.sum_profit] = 0.0
+        ord_df.at[pos, const.sum_profit_prc] = 0.0
     else:
-        d_price = beg_val - end_val
+        ord_df.at[pos,const.close_dt_col_name] = end_dt
+        ord_df.at[pos,const.close_price_col_name] = end_val
 
-    d_price_prc = (d_price/beg_val)*100.0
-    p_prc = d_price_prc - 0.4
-    p = (p_prc/100.0)*beg_val
+        if o_buy:
+            d_price = end_val - beg_val
+        else:
+            d_price = beg_val - end_val
 
-    ord_df.at[pos,const.delta_price] = d_price
-    ord_df.at[pos,const.delta_price_prc] = d_price_prc
-    ord_df.at[pos,const.profit] = p
-    ord_df.at[pos,const.profit_prc] = p_prc
+        d_price_prc = (d_price/beg_val)*100.0
+        p_prc = d_price_prc - 0.4
+        p = (p_prc/100.0)*beg_val
 
-    prev = pos - 1
-    if prev >= 0:
-        ord_df.at[pos, const.sum_profit] = ord_df.at[prev, const.sum_profit] + p
-        ord_df.at[pos, const.sum_profit_prc] = ord_df.at[prev, const.sum_profit_prc] + p_prc
-    else:
-        ord_df.at[pos, const.sum_profit] = p
-        ord_df.at[pos, const.sum_profit_prc] = p_prc
+        ord_df.at[pos,const.delta_price] = d_price
+        ord_df.at[pos,const.delta_price_prc] = d_price_prc
+        ord_df.at[pos,const.profit] = p
+        ord_df.at[pos,const.profit_prc] = p_prc
+
+        prev = pos - 1
+        if prev >= 0:
+            ord_df.at[pos, const.sum_profit] = ord_df.at[prev, const.sum_profit] + p
+            ord_df.at[pos, const.sum_profit_prc] = ord_df.at[prev, const.sum_profit_prc] + p_prc
+        else:
+            ord_df.at[pos, const.sum_profit] = p
+            ord_df.at[pos, const.sum_profit_prc] = p_prc
 
     return ord_df
 
@@ -120,6 +128,30 @@ def get_curr_prev_minute_utc():
     return c_utc, p_utc
 
 
+def check_for_order_open(ord_df):
+
+    len = ord_df[const.type_col_name].size
+    pos = len - 1
+
+    close_dt = ord_df.at[pos, const.close_dt_col_name]
+    ord_type = ord_df.at[pos, const.type_col_name]
+    if close_dt == 0.0: # last order not closed
+        order_now = True
+        if ord_type==const.order_type_buy:
+            order_buy = True
+        else:
+            order_buy = False
+        beg_dt = ord_df.at[pos, const.open_dt_col_name]
+        beg_val = ord_df.at[pos, const.open_price_col_name]
+    else:
+        order_now = False
+        order_buy = False
+        beg_dt = 0.0
+        beg_val = 0.0
+
+    return order_now, order_buy, beg_dt, beg_val
+
+
 def update_eq_order(out_df, ord_df):
 
     out_len = out_df[const.dt_col_name].size
@@ -130,10 +162,8 @@ def update_eq_order(out_df, ord_df):
 
     if (last_dt_utc >= prev_min_utc) & (last_dt_utc <= curr_min_utc):
 
-        order_now = False
-        ord_open = False
-        order_buy = True
         ord_change = False
+        order_now, order_buy, beg_dt, beg_val = check_for_order_open(ord_df)
 
         mean_value = out_df[const.value_col_name].mean()
         min_value = out_df[const.value_col_name].min()
@@ -142,24 +172,20 @@ def update_eq_order(out_df, ord_df):
         out_df.at[0, const.order_col_name] = mean_value
         out_df.at[1, const.order_col_name] = mean_value
 
-        beg_dt = 0.0
-        beg_val = 0.0
-
-        order_now, order_buy, ord_open, ord_change = check_order_open_close(
-            out_df, x, order_now, order_buy, ord_open, ord_change)
-
+        order_now, order_buy, ord_change = check_order_open_close(out_df, x, order_now, order_buy, ord_change)
         out_df = fill_equation_values(out_df, x, order_now, order_buy, mean_value, min_value, max_value)
 
         if ord_change:
-            if ord_open:
+            if order_now:
                 beg_dt = out_df.at[x, const.dt_col_name]
                 beg_val = out_df.at[x, const.value_col_name]
+                ord_df = fill_order_values(ord_df, order_now, order_buy, beg_dt, beg_val, 0.0, 0.0)
             else:
                 end_dt = out_df.at[x, const.dt_col_name]
                 end_val = out_df.at[x, const.value_col_name]
-                ord_df = fill_order_values(ord_df, order_buy, beg_dt, beg_val, end_dt, end_val)
+                ord_df = fill_order_values(ord_df, order_now, order_buy, beg_dt, beg_val, end_dt, end_val)
 
-    return out_df
+    return out_df, ord_df
 
 
 def update_orders_by_symbol(symbol_str):
@@ -178,7 +204,7 @@ def update_orders_by_symbol(symbol_str):
                                        const.sum_profit, const.sum_profit_prc])
     print('..load ord.', end='')
 
-    update_eq_order(eq_df, ord_df)
+    out_df, ord_df = update_eq_order(eq_df, ord_df)
     print('..update ord.', end='')
 
     ord_df.to_csv(ord_file_name, index=False, header=True)
