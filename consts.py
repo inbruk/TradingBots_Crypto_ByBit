620abd762a3e8f33fdfe66beb5f66cccf9d4d716
inbruk <35333872+inbruk@users.noreply.github.com>
2021-05-13 04:03:09 +0300

    .....................................r..............t.....

--- consts.py
+++ consts.py
@@ -22,7 +22,7 @@ const.ORDERS = 'orders'
 const.avg7_hwnd = 4
 const.avg31_hwnd = 16
 const.avg181_hwnd = 128
-const.avg1441_hwnd = 256
+const.avg1441_hwnd = 300
 
 const.open_col_name = 'open'
 const.close_col_name = 'close'
@@ -75,8 +75,8 @@ const.order_time_in_force_immediate_or_cancel = 'ImmediateOrCancel'
 const.order_time_in_force_fill_or_kill = 'FillOrKill'
 const.order_time_in_force_post_only = 'PostOnly'
 
-const.order_stop_lost_koef_buy = 0.98
-const.order_stop_lost_koef_sell = 1.02
+const.order_stop_lost_koef_buy = 0.99
+const.order_stop_lost_koef_sell = 1.01
 
 const.order_take_profit_koef_buy = 1.01
 const.order_take_profit_koef_sell = 0.99
