d2d323699dd185a9b3a8842279d007c25f6b214f
inbruk <35333872+inbruk@users.noreply.github.com>
2021-05-16 22:51:38 +0300

    .....................................r..............t.....+....+t............+.......+

--- consts.py
+++ consts.py
@@ -79,8 +79,8 @@ const.order_time_in_force_immediate_or_cancel = 'ImmediateOrCancel'
 const.order_time_in_force_fill_or_kill = 'FillOrKill'
 const.order_time_in_force_post_only = 'PostOnly'
 
-const.order_stop_lost_koef_buy = 0.98
-const.order_stop_lost_koef_sell = 1.02
+const.order_stop_lost_koef_buy = 0.95
+const.order_stop_lost_koef_sell = 1.05
 
 const.order_take_profit_koef_buy = 1.02
 const.order_take_profit_koef_sell = 0.98
